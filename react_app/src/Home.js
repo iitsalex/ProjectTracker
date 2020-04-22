ebd3b43ecde801d214c744345c2986030a7f64e7
Alex Khalilieh <iitsalex@hotmail.com>
2020-04-21 18:37:03 -0700

    sign up page redirects and homepage

--- react_app/src/Home.js
+++ react_app/src/Home.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react'
-
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap"
 class Home extends Component {
   render() {
     return (
       <div>
       <img src={ require('./assets/images/Pivot_logo.png') } />
-
       <h1>Your Project Tracking Solution</h1>
+      <br/>
+      <Button bsSize="large" type="submit">
+        <Link to="/users" style={{ color: '#FFF' }}>Sign Up</Link>
+      </Button>
       </div>
     );
   }
