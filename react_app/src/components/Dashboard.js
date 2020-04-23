a4baecbf4e50b577c39d1c67915f82ea036f5a9c
Alex Khalilieh <iitsalex@hotmail.com>
2020-04-22 21:44:04 -0700

    drag n drop dashboard

--- react_app/src/components/Dashboard.js
+++ react_app/src/components/Dashboard.js
@@ -1,94 +1,110 @@
-import React, {Component} from 'react'
-import { Link } from "react-router-dom";
-import { Card, Table } from "react-bootstrap";
-class Dashboard extends Component {
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-  // may need a cardCreate function up here that is passed below in order
-  //to pass in a list of cards
+// fake data generator
+const getItems = count =>
+  Array.from({ length: count }, (v, k) => k).map(k => ({
+    id: `item-${k}`,
+    content: `item ${k}`
+  }));
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  width: 250
+});
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: getItems(10)
+    };
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
+
+  onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      items
+    });
+  }
+
+  // Normally you would want to split things out into separate components.
+  // But in this example everything is just done in one place for simplicity
   render() {
     return (
       <div>
-      <h1>Dashboard</h1>
-      <br/>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>New</th>
-            <th>In Progress</th>
-            <th>Done</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-
-            <td>
-              <Card>
-                <Card.Body>
-                  <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
-                  <Card.Text>
-                    God help this project.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              <Card>
-                <Card.Body>
-                  <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
-                  <Card.Text>
-                    God help this project.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </td>
-
-            <td>
-              <Card>
-                <Card.Body>
-                  <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
-                  <Card.Text>
-                    God help this project.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              <Card>
-                <Card.Body>
-                  <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
-                  <Card.Text>
-                    God help this project.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </td>
-
-            <td>
-              <Card>
-                <Card.Body>
-                  <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
-                  <Card.Text>
-                    God help this project.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              <Card>
-                <Card.Body>
-                  <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
-                  <Card.Text>
-                    God help this project.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
+        <h1> Dashboard </h1>
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+              >
+                {this.state.items.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
+                        )}
+                      >
+                        {item.content}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </div>
     );
   }
 }
 
+// Put the thing into the DOM!
 export default Dashboard
