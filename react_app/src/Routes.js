63ea0e9ca2d3e1098f893583d50ee4d703e31e35
Lovejit <kharodl@users.noreply.github.com>
2020-04-22 20:00:09 -0700

    fixed warnings and redid login a bit

--- react_app/src/Routes.js
+++ react_app/src/Routes.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
 
-import MainNavbar from './MainNavbar';
 import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
