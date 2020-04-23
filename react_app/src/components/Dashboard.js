1e929d86427f1975663d8702c1f0f9cffcb8ba61
Alex Khalilieh <iitsalex@hotmail.com>
2020-04-22 20:36:22 -0700

    Dashboard table

--- react_app/src/components/Dashboard.js
+++ react_app/src/components/Dashboard.js
@@ -1,55 +1,91 @@
 import React, {Component} from 'react'
 import { Link } from "react-router-dom";
-import { Button, Card, CardColumns, CardHeader, Nav, ListGroup } from "react-bootstrap";
+import { Card, Table } from "react-bootstrap";
 class Dashboard extends Component {
+
+  // may need a cardCreate function up here that is passed below in order
+  //to pass in a list of cards
   render() {
     return (
       <div>
       <h1>Dashboard</h1>
       <br/>
-      <Card>
-        <Card.Header>
-          <Nav variant="tabs" defaultActiveKey="#first">
-            <Nav.Item>
-              <Nav.Link href="#first">Active</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#link">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#disabled" disabled>
-                Disabled
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Card.Header>
-        <Card.Body>
-        <CardColumns>
-          <Card style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
-              <Card.Text>
-                God help this project.
-              </Card.Text>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Title>As a developer, idfk what the fuck im doing</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">Sprint 2</Card.Subtitle>
-              <Card.Text>
-                holy fuck this is ugly but functional?
-              </Card.Text>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
-          </Card>
-        </CardColumns>
-        </Card.Body>
-      </Card>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>New</th>
+            <th>In Progress</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+
+            <td>
+              <Card>
+                <Card.Body>
+                  <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
+                  <Card.Text>
+                    God help this project.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Body>
+                  <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
+                  <Card.Text>
+                    God help this project.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </td>
+
+            <td>
+              <Card>
+                <Card.Body>
+                  <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
+                  <Card.Text>
+                    God help this project.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Body>
+                  <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
+                  <Card.Text>
+                    God help this project.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </td>
+
+            <td>
+              <Card>
+                <Card.Body>
+                  <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
+                  <Card.Text>
+                    God help this project.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Body>
+                  <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
+                  <Card.Text>
+                    God help this project.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
       </div>
     );
   }
---
+++ react_app/src/components/DashboardV1.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react'
+import { Link } from "react-router-dom";
+import { Button, Card, CardColumns, CardHeader, Nav, ListGroup, CardDeck } from "react-bootstrap";
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Dashboard</h1>
+      <br/>
+      <CardDeck style={{display: 'flex', flexDirection: 'column'}}>
+        <Card style={{ width: '18rem', flex: 1}}>
+          <Card.Body>
+            <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
+            <Card.Text>
+              God help this project.
+            </Card.Text>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem', flex: 1}}>
+          <Card.Body>
+            <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
+            <Card.Text>
+              God help this project.
+            </Card.Text>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
+      </CardDeck>
+      <Card>
+        <Card.Header>
+          <Nav variant="tabs" defaultActiveKey="#first">
+            <Nav.Item>
+              <Nav.Link href="#first">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#disabled" disabled>
+                Disabled
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Card.Header>
+        <Card.Body>
+        <CardColumns>
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>As a user I want a working Bootleg Jira</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Sprint 1: 04/22 - 04/28</Card.Subtitle>
+              <Card.Text>
+                God help this project.
+              </Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>As a developer, idfk what the fuck im doing</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Sprint 2</Card.Subtitle>
+              <Card.Text>
+                holy fuck this is ugly but functional?
+              </Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+        </CardColumns>
+        </Card.Body>
+      </Card>
+      </div>
+    );
+  }
+}
+
+export default Dashboard
