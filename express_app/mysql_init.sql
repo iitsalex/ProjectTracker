00878a20270274fc2b4aff283ed25be1b13c022d
Lovejit <kharodl@users.noreply.github.com>
2020-04-23 00:42:11 -0700

    added task statuses

--- express_app/mysql_init.sql
+++ express_app/mysql_init.sql
@@ -40,6 +40,7 @@ CREATE TABLE tasks(
     project_id INT NOT NULL,
     name VARCHAR(100) NOT NULL,
     description TEXT,
+    status VARCHAR(50),
     created DATE,
     PRIMARY KEY (id),
     FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE,
