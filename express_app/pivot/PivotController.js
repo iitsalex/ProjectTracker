5b36add8a588b47bd1bfa6d132ca363b06aaa5c6
Lovejit <kharodl@users.noreply.github.com>
2020-04-24 20:24:58 -0700

    Changed calls for safety and user fetch

--- express_app/pivot/PivotController.js
+++ express_app/pivot/PivotController.js
@@ -202,7 +202,7 @@ router.post('/teams/join', function (req, res) {
                                         res.status(400).json(err);
                                     }
                                 });
-                                res.status(200).json(req.body);
+                                res.status(200).json(user[0]);
                             }
                         });
                     }
