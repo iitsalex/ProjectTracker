af0b6f5c61792bcb215d0ede338dca825093a2cd
Alex Khalilieh <iitsalex@hotmail.com>
2020-04-24 22:55:21 -0700

    Create Task updates

--- react_app/src/components/projects/tasks/CreateTask.js
+++ react_app/src/components/projects/tasks/CreateTask.js
@@ -8,6 +8,11 @@ class CreateTask extends Component {
     this.state = {
       name: '',
       description: '',
+      teams: [],
+      projects: [],
+      team_id: '',
+      project_id: '',
+      projects_enabled: false
     };
   }
 
@@ -39,9 +44,42 @@ class CreateTask extends Component {
     });
   }
 
+  handleInputChange = (event) => {
+    const {value, name} = event.target;
+    this.setState({[name]: value});
+    fetch('/api/projects/team/' + this.state.team_id).then(res => {
+      if (res.status === 200) {
+        return('Auth ok');
+      } else if (res.status === 401) {
+        return('Auth failed');
+      }
+    }).then(data => this.setState(
+      { projects: data,
+        projects_enabled: true})
+    ).catch(console.error);
+  }
+
   render() {
     return (
       <Form onSubmit={this.onSubmit}>
+        <FormGroup>
+          <FormControl as="select" name="team_id" value={this.state.team_id} onChange={this.handleInputChange} maxLength="100" autoComplete="off" required="required">
+            {
+              this.state.projects.map(project => {
+                return <option key={project.id} value={project.id}>{project.name}</option>
+              })
+            }
+          </FormControl>
+        </FormGroup>
+        <FormGroup>
+          <FormControl as="select" name="team_id" placeholder="Enter Team Member's Email" value={this.state.team_id} onChange={this.handleInputChange} maxLength="100" autoComplete="off" required="required">
+            {
+              this.state.teams.map(team => {
+                return <option key={team.id} value={team.id}>{team.name}</option>
+              })
+            }
+          </FormControl>
+        </FormGroup>
         <FormGroup>
           <FormLabel className="text-muted">Task Name</FormLabel>
           <FormControl
