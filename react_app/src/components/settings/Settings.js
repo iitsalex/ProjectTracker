177126ed679d90be1d8a0a53fbf7d3cf0fe374c0
Lovejit <kharodl@users.noreply.github.com>
2020-04-27 00:13:17 -0700

    heading size changes

--- react_app/src/components/settings/Settings.js
+++ react_app/src/components/settings/Settings.js
@@ -5,12 +5,10 @@ class Settings extends Component {
   render() {
     return (
       <div>
-      <br/>
-      <h1>Settings</h1>
-      <br/>
-      <Button type="submit">
-        <Link to="/settings" style={{ color: '#FFF' }}>Temp Button</Link>
-      </Button>
+        <h3>Settings</h3>
+        <Button type="submit">
+          <Link to="/settings" style={{ color: '#FFF' }}>Temp Button</Link>
+        </Button>
       </div>
     );
   }
