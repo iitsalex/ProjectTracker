9b115fb1c7e7b01ee558d2f35a75862923fa4f4c
Lovejit <kharodl@users.noreply.github.com>
2020-04-27 01:03:25 -0700

    tasks are goin in

--- react_app/src/components/projects/Dashboard.js
+++ react_app/src/components/projects/Dashboard.js
@@ -1,14 +1,98 @@
 import React, { Component } from "react";
-
-// https://codesandbox.io/s/-w5szl old source for the draggable one, not really worth using but maybe you can get an idea from interval
+import { CardColumns, Card, Col } from "react-bootstrap";
 
 class Dashboard extends Component {
+  _isMounted = false;
 
-    render() {
-        return (
-          <div></div>
-        );
+  constructor() {
+    super();
+    //Set default message
+    this.state = {
+      tasks: [],
+      newTasks: [],
+      ipTasks: [],
+      doneTasks: []
     }
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+    fetch('/api/tasks/project/' + this.props.data.project_id).then(res => {
+      if (res.status === 200) {
+        return res.json();
+      } else {
+        const error = new Error(res.error);
+        throw error;
+      }
+    }).then(data => {
+      if (this._isMounted) {
+        this.setState({ tasks: data }, this.distributeTasks());
+      }
+    }).catch(console.error);
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  distributeTasks = () => {
+    for (const task in this.tasks) {
+      switch (task.status) {
+        case 'New':
+          this.newTasks.push(task);
+          break;
+        case 'In Progess':
+          this.ipTasks.push(task);
+          break;
+        case 'Done':
+          this.doneTasks.push(task);
+          break;
+      }
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <CardColumns>
+          {this.state.newTasks.map(task => {
+            return  <Card>
+                      <Card.Body>
+                        <Card.Title>{task.name}</Card.Title>
+                        <Card.Text>
+                          {task.description}
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+          })}
+        </CardColumns>
+        <CardColumns>
+          {this.state.ipTasks.map(task => {
+            return  <Card>
+                      <Card.Body>
+                        <Card.Title>{task.name}</Card.Title>
+                        <Card.Text>
+                          {task.description}
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+          })}
+        </CardColumns>
+        <CardColumns>
+          {this.state.doneTasks.map(task => {
+            return  <Card>
+                      <Card.Body>
+                        <Card.Title>{task.name}</Card.Title>
+                        <Card.Text>
+                          {task.description}
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+          })}
+        </CardColumns>
+      </>
+    );
+  }
 }
 
 // Put the thing into the DOM!
