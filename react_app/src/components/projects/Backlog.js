9575a516988a2f6432d1ff015fd6572aba285a73
Alex Khalilieh <iitsalex@hotmail.com>
2020-04-27 17:46:05 -0700

    Backlog View + Card change

--- react_app/src/components/projects/Backlog.js
+++ react_app/src/components/projects/Backlog.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from 'react-bootstrap'
+import { Button, Container, Row, Col, Card } from 'react-bootstrap'
 import ModalTemplate from "../ModalTemplate";
 import CreateTask from "./tasks/CreateTask";
 
@@ -10,23 +10,45 @@ class Backlog extends React.Component {
       modalShow: false
     };
   }
-
+// this.props.data.tasks
   render() {
     return (
-      <>
-        <Button variant="primary" onClick={() => this.setState({modalShow: true})}>
-          Create Task
-        </Button>
+      <div>
+        <h3> Backlog </h3>
+        <>
+          <Button variant="primary" onClick={() => this.setState({modalShow: true})}>
+            Create Task
+          </Button>
 
-        <ModalTemplate
-          show={this.state.modalShow}
-          onHide={() => this.setState({modalShow: false})}
-          title="Create Task"
-          component={CreateTask}
-          project_id={this.props.data.project_id}
-          updateTasks={this.props.updateTasks}
-        />
+          <ModalTemplate
+            show={this.state.modalShow}
+            onHide={() => this.setState({modalShow: false})}
+            title="Create Task"
+            component={CreateTask}
+            project_id={this.props.data.project_id}
+            updateTasks={this.props.updateTasks}
+          />
       </>
+      <Container>
+        <Row>
+          <Col lg>
+            {this.props.data.all_tasks.reverse().map(task =>
+              task.status === 'Done' ? '' :
+                <Card key={task.id}>
+                  <Card.Body>
+                    <Card.Title><b>{task.name}</b></Card.Title>
+                    <Card.Subtitle>Task Number: {task.id}</Card.Subtitle>
+                    <Card.Text>
+                      {task.description.substring(0,50)}
+                      {task.description.length > 50 ? '...' : ''}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+            )}
+          </Col>
+        </Row>
+      </Container>
+  </div>
     );
   }
 }
