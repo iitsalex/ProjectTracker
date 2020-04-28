8fd59fe0dbac235dce2ffc6734ebb71303480343
Alex Khalilieh <iitsalex@hotmail.com>
2020-04-27 18:03:21 -0700

    Backlogs view

--- react_app/src/components/projects/Backlog.js
+++ react_app/src/components/projects/Backlog.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Button, Container, Row, Col, Card } from 'react-bootstrap'
 import ModalTemplate from "../ModalTemplate";
 import CreateTask from "./tasks/CreateTask";
+import ViewTask from "./tasks/ViewTask";
+
 
 class Backlog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modalShow: false
+      modalCreate: false,
+      modalView: false,
+      task: '',
+      tasks: this.props.data.all_tasks.reverse()
     };
   }
 // this.props.data.tasks
@@ -16,13 +21,13 @@ class Backlog extends React.Component {
       <div>
         <h3> Backlog </h3>
         <>
-          <Button variant="primary" onClick={() => this.setState({modalShow: true})}>
+          <Button variant="primary" onClick={() => this.setState({modalCreate: true})}>
             Create Task
           </Button>
 
           <ModalTemplate
-            show={this.state.modalShow}
-            onHide={() => this.setState({modalShow: false})}
+            show={this.state.modalCreate}
+            onHide={() => this.setState({modalCreate: false})}
             title="Create Task"
             component={CreateTask}
             project_id={this.props.data.project_id}
@@ -30,14 +35,26 @@ class Backlog extends React.Component {
           />
       </>
       <Container>
+        <ModalTemplate
+          show={this.state.modalView}
+          onHide={() => this.setState({modalView: false})}
+          title={this.state.task.name}
+          component={ViewTask}
+          task={this.state.task}
+          project_id={this.props.data.project_id}
+          updateTasks={this.props.updateTasks}
+        />
         <Row>
           <Col lg>
-            {this.props.data.all_tasks.reverse().map(task =>
+            {this.state.tasks.map(task =>
               task.status === 'Done' ? '' :
-                <Card key={task.id}>
+                <Card key={task.id} onClick={() => this.setState({
+                    task: task,
+                    modalView: true
+                  })}>
                   <Card.Body>
                     <Card.Title><b>{task.name}</b></Card.Title>
-                    <Card.Subtitle className="text-muted pad-em">Date Created: {task.created.substring(0,10)}</Card.Subtitle>
+                    <Card.Subtitle className="text-muted pad-em-bottom">Date Created: {task.created.substring(0,10)}</Card.Subtitle>
                     <Card.Text className="slight-muted">
                       {task.description.substring(0,50)}
                       {task.description.length > 50 ? '...' : ''}
