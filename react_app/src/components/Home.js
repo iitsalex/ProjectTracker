a9f36b7fc8fdb3c71846c1bda82ec8aa0aea85bc
Lovejit <kharodl@users.noreply.github.com>
2020-04-29 14:31:58 -0700

    Update Home.js

--- react_app/src/components/Home.js
+++ react_app/src/components/Home.js
@@ -5,15 +5,17 @@ import FadeIn from 'react-fade-in';
 class Home extends Component {
   render() {
     return (
-      <FadeIn delay='200'>
-        <img src={ require('../assets/images/Pivot_logo.png') } alt="Pivot Logo" />
-        <h1>Your Project Tracking Solution</h1>
-        <br/>
-        {this.props.is_auth ?
-          <p>Welcome {this.props.user.fname + ' ' + this.props.user.lname}</p> :
-          <Button variant='info' href="/signup"type="submit">Sign Up</Button>
-        }
-      </FadeIn>
+      <header>
+        <FadeIn delay='200'>
+          <img src={ require('../assets/images/Pivot_logo.png') } alt="Pivot Logo" />
+          <h1>Your Project Tracking Solution</h1>
+          <br/>
+          {this.props.is_auth ?
+            <p>Welcome {this.props.user.fname + ' ' + this.props.user.lname}</p> :
+            <Button variant='info' href="/signup"type="submit">Sign Up</Button>
+          }
+        </FadeIn>
+      </header>
     );
   }
 }
