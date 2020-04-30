8757f4ee217209657ec9743218e23b0ca7d60b76
Lovejit <kharodl@users.noreply.github.com>
2020-04-30 12:12:20 -0700

    wip sql changes
    planning to fit in sprints somehow

---
+++ react_app/api/mysql_init.sql
@@ -0,0 +1,83 @@
+# MySQL commands for reference
+## Setting up your mysql database
+
+DROP DATABASE IF EXISTS pivot;
+
+CREATE DATABASE pivot;
+
+USE pivot;
+
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(100) NOT NULL,
+    password CHAR(60) NOT NULL,
+    fname VARCHAR(50) NOT NULL,
+    lname VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE projects(
+    id INT NOT NULL AUTO_INCREMENT,
+    owner_id INT, -- May be null
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    created DATE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE SET NULL
+);
+
+CREATE TABLE teams(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    size INT NOT NULL,
+    lead_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tasks(
+    id INT NOT NULL AUTO_INCREMENT,
+    owner_id INT, -- May be null
+    assignee_id INT, -- May be null
+    project_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    status VARCHAR(20) NOT NULL,
+    points INT NOT NULL,
+    state INT DEFAULT 1, -- 0: in backlog, 1: in sprint,  2: completed not in sprint
+    created DATE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE SET NULL,
+    FOREIGN KEY (assignee_id) REFERENCES users(id) ON DELETE SET NULL,
+    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_team(
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    team_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE
+);
+
+-- CREATE TABLE project_sprint(
+--     id INT NOT NULL AUTO_INCREMENT,
+--     user_id INT NOT NULL,
+--     task_id INT NOT NULL,
+--     PRIMARY KEY (id),
+--     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+--     FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE
+-- );
+
+CREATE TABLE team_project(
+    id INT NOT NULL AUTO_INCREMENT,
+    team_id INT NOT NULL,
+    project_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE,
+    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
+);
+
+CREATE USER IF NOT EXISTS 'pivot'@'%' IDENTIFIED BY 'pivot!Node.js';
+GRANT ALL PRIVILEGES ON pivot.* TO 'pivot'@'%';
+FLUSH PRIVILEGES;
