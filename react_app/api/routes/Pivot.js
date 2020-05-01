725be8be1137135dd6caa2c8771b95139506ebe5
Lovejit <kharodl@users.noreply.github.com>
2020-04-30 23:23:50 -0700

    fixed something i forgot to change back
    was breaking task creation

--- react_app/api/routes/Pivot.js
+++ react_app/api/routes/Pivot.js
@@ -88,7 +88,7 @@ var Pivot = {
 
   // Tasks
   createtask: function(data, uid, sprint_id, today, callback) {
-    return db.query('INSERT INTO tasks (owner_id, assignee_id, project_id, name, description, status, state, points, created, sprint_id)  VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)', [
+    return db.query('INSERT INTO tasks (owner_id, assignee_id, project_id, name, description, status, points, created, sprint_id)  VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)', [
       uid,
       data.assignee_id,
       data.project_id,
